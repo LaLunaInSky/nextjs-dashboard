e531614bd9565a13d001e20452079d36d06f4c09
LaLunaInSky <moonaflordapele@gmail.com>
2025-01-28 10:16:02 -0300

    28/01/2025 10:15

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,8 @@
-import Table from "@/app/ui/customers/table"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "Customers"
+}
 
 export default function Page(){
     return (
